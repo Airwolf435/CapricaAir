e92e0d8d4c94541fa3889ce6de24e3b6d789dae0
John Keenan <jgknomad@gmail.com>
2024-02-13 23:06:01 -0500

    Implement first pass of theme block hooks

--- plugins/caprica-air-core-block-hooks/assets/js/caprica-air-core-block-hooks.js
+++ plugins/caprica-air-core-block-hooks/assets/js/caprica-air-core-block-hooks.js
@@ -3,4 +3,84 @@ wp.domReady(function (){
         name: 'test-style',
         label: 'Test Style',
     } );
+
+    wp.blocks.registerBlockStyle( 'core/paragraph', {
+        name: 'text-input',
+        label: 'Input Box',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/preformatted', {
+        name: 'message-box',
+        label: 'Message Box',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/heading', {
+        name: 'default-heading',
+        label: 'Default Heading',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/list', {
+        name: 'caprica-list',
+        label: 'Caprica List',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/preformatted', {
+        name: 'caprica-preformat',
+        label: 'Caprica Preformatted',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/pullquote', {
+        name: 'caprica-pullquote',
+        label: 'Caprica Pullquote',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/table', {
+        name: 'caprica-table',
+        label: 'Caprica Table',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/verse', {
+        name: 'caprica-verse',
+        label: 'Caprica Verse',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/footnotes', {
+        name: 'caprica-footnotes',
+        label: 'Caprica Footnotes',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/code', {
+        name: 'caprica-code',
+        label: 'Caprica Code',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/details', {
+        name: 'caprica-details',
+        label: 'Caprica Details',
+        isDefault: true
+    } );
+    
+    wp.blocks.registerBlockStyle( 'core/quote', {
+        name: 'caprica-quote',
+        label: 'Caprica Quote',
+        isDefault: true
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/video', {
+        name: 'video-polygotal',
+        label: 'Polygotal',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/heading', {
+        name: 'heading-polygotal',
+        label: 'Polygotal',
+    } );
 })
