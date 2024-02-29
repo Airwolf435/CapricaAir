07d601c50a0fe72a118f53ed956967a72de6750e
John Keenan <jgknomad@gmail.com>
2024-02-29 05:25:01 -0500

    update post types plugin file to include rewrite flush

--- plugins/caprica-air-post-types/caprica-air-post-types.php
+++ plugins/caprica-air-post-types/caprica-air-post-types.php
@@ -3,7 +3,7 @@
  * Plugin Name: Caprica Air Post Types
  */
 
-function caprica_air_custom_post_stype(){
+function caprica_air_custom_posts_type(){
     register_post_type('capricaair_events', [
         "labels" => [
             "name" => "events",
@@ -11,6 +11,19 @@ function caprica_air_custom_post_stype(){
         ],
         "public" => true,
         "has_archive" => true,
+        'show_in_rest' => true,
+        "supports" => ['title', 'editor', 'revisions', 'author', 'excerpt'],
+        "rewrite" => [
+            "slug" => "events"
+        ]
     ]
 	);
 }
+
+add_action('init', 'caprica_air_custom_posts_type');
+
+function caprica_air_rewrite_flush(){
+    caprica_air_custom_posts_type();
+    flush_rewrite_rules();
+}
+add_action( 'init', 'caprica_air_rewrite_flush' );
